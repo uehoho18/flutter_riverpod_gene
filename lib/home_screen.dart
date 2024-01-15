ca8b868de78826b7f34625acdd0545b62b7d04fe
tomohiro ueno <hagurono9@gmail.com>
2024-01-15 13:59:29 +0900

    記述構成を変更

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_gene/main.dart';
 
-class MyHomePage extends ConsumerWidget {
-  const MyHomePage({super.key});
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final name = ref.watch(nameProvider);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(children: [
-        Center(
-          child: Text(name),
+    return Consumer(builder: (context, ref, child) {
+      final name = ref.watch(nameProvider);
+      return Scaffold(
+        appBar: AppBar(),
+        body: Consumer(
+          builder: (context, ref, child) {
+            return Column(
+              children: [
+                Center(
+                  child: Text(name),
+                ),
+              ],
+            );
+          },
         ),
-      ]),
-    );
+      );
+    });
   }
 }
