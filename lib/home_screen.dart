075a82a3e718ced9bf7e51d202f1b7668227b4af
tomohiro ueno <hagurono9@gmail.com>
2024-01-15 12:13:09 +0900

    add

---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_gene/main.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    name = 'uetomo';
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(children: [
+        Center(
+          child: Text(''),
+        ),
+      ]),
+    );
+  }
+}
